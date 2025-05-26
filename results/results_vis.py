33fa9d0cba993fb477e476a79603cd1dd2cac15f
LewisDeZoete <lewis.dezoete@gmail.com>
2025-05-26 11:29:52 +1000

    Functions for loss and accuracy comparison graphing

--- results/results_vis.py
+++ results/results_vis.py
@@ -4,21 +4,16 @@ import torch
 import os
 import os.path as osp
 
-# dataset = 'nturgbd'
-# evaluation = 'CV'
-dataset = 'ucf101'
-evaluation = ''
+dataset = 'nturgbd'
+evaluation = 'CS'
+# dataset = 'ucf101'
+# evaluation = '1'
 
-if dataset == 'ucf101':
-    checkpoint_base_path = f'results/{dataset}/infogcn_base.pt'
-    checkpoint_abs_path = f'results/{dataset}/infogcn_abs.pt'
-    checkpoint_avg_path = f'results/{dataset}/infogcn_avg.pt'
-    checkpoint_cnn_path = f'results/{dataset}/infogcn_cnn.pt'
-else:
-    checkpoint_base_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_base.pt'
-    checkpoint_abs_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_abs.pt'
-    checkpoint_avg_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_avg.pt'
-    checkpoint_cnn_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_cnn.pt'
+checkpoint_base_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_base.pt'
+checkpoint_abs_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_abs.pt'
+checkpoint_avg_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_avg.pt'
+checkpoint_cnn_path = f'results/{dataset}/{evaluation}/{dataset}_{evaluation}_cnn.pt'
+
 checkpoint_base = torch.load(checkpoint_base_path, map_location='cpu')
 checkpoint_abs = torch.load(checkpoint_abs_path, map_location='cpu')
 checkpoint_avg = torch.load(checkpoint_avg_path, map_location='cpu')
@@ -37,53 +32,66 @@ plot_root = 'results/plots/'
 os.makedirs(plot_root, exist_ok=True) # Make dir if it doesn't exist
 
 # Evaluation string and Dataset string dictionaries for plot titles
-eval_str_dict = {'CS': 'Cross Subject', 'CV': 'Cross View'}
+eval_str_dict = {'CS': 'Cross Subject', 'CV': 'Cross View', 
+                 '1': 'Evaluation 1', '2': 'Evaluation 2', '3': 'Evaluation 3'}
 dataset_str_dict = {'nturgbd': 'NTU RGB+D', 'nturgbd120': 'NTU RGB+D 120', 'ucf101':'UCF-101'}
-if dataset == 'ucf101':
-    fig_title = f'{dataset_str_dict[dataset]}'
-    save_root = osp.join(plot_root,f'{dataset}')
-    print(f'Plotting results for {dataset_str_dict[dataset]}')
-else:
-    fig_title = f'{dataset_str_dict[dataset]} - {eval_str_dict[evaluation]}'
-    save_root = osp.join(plot_root, f'{dataset}-{evaluation}/')
-    print(f'Plotting results for {dataset_str_dict[dataset]} - {eval_str_dict[evaluation]} evaluation')
 
-# # ------------------------------
-# #   Loss comparison graphing
-# # ------------------------------
+fig_title = f'{dataset_str_dict[dataset]} - {eval_str_dict[evaluation]}'
+print(f'Plotting results for {dataset_str_dict[dataset]} - {eval_str_dict[evaluation]} evaluation')
 
-# # Plot the results
-# fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(25,7))
-# fig.suptitle(f'Loss Comparison - {fig_title}', fontsize=30)
-# for ax in (ax1, ax2, ax3):
-#     ax.tick_params(axis='both', which='major', labelsize=20)
-#     ax.set_xlabel('Epoch', fontsize=20)
-# ax1.set_ylabel('Loss', fontsize=20)
+# Create a directory, ignoring if it already exists
+save_root = osp.join(plot_root, f'{dataset}-{evaluation}/')
+os.makedirs(save_root, exist_ok=True)
 
-# # Plot class loss
-# ax1.set_title('Classification Loss', fontsize = 25)
-# ax1.plot(x, results_base['train_cls_loss'], label='Base', color='tab:olive')
-# ax1.plot(x, results_abs['train_cls_loss'], label='Absolute flow', color='tab:green')
-# ax1.plot(x, results_avg['train_cls_loss'], label='Average flow', color='tab:pink')
-# ax1.plot(x, results_cnn['train_cls_loss'], label='CNN learning', color='tab:blue')
-# ax1.legend()
+# ------------------------------
+#   Loss comparison graphing
+# ------------------------------
 
-# # Plot feature loss
-# ax2.set_title('Feature Loss', fontsize = 25)
-# ax2.plot(x, results_base['train_feature_loss'], label='Base', color='tab:olive')
-# ax2.plot(x, results_abs['train_feature_loss'], label='Absolute flow', color='tab:green')
-# ax2.plot(x, results_avg['train_feature_loss'], label='Average flow', color='tab:pink')
-# ax2.plot(x, results_cnn['train_feature_loss'], label='CNN', color='tab:blue')
+def plot_loss_comp(fig_title: str, x, save_root,
+              results_base, results_abs, results_avg, results_cnn):
+    """
+    Plot the loss comparison graph for different models.
+    Args:
+        fig_title (str): Title of the figure.
+        x (array): Epochs.
+        results_base (dict): Results for the base model.
+        results_abs (dict): Results for the absolute flow model.
+        results_avg (dict): Results for the average flow model.
+        results_cnn (dict): Results for the CNN model.
+    """
+    # Create figure and axes
+    fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(25,7))
+    fig.suptitle(f'Loss Comparison - {fig_title}', fontsize=30)
+    for ax in (ax1, ax2, ax3):
+        ax.tick_params(axis='both', which='major', labelsize=20)
+        ax.set_xlabel('Epoch', fontsize=20)
+    ax1.set_ylabel('Loss', fontsize=20)
 
-# # Plot reconstruction loss
-# ax3.set_title('Reconstruction Loss', fontsize = 25)
-# ax3.plot(x, results_base['train_recon_loss'], label='Base', color='tab:olive')
-# ax3.plot(x, results_abs['train_recon_loss'], label='Average flow', color='tab:green')
-# ax3.plot(x, results_avg['train_recon_loss'], label='Average flow', color='tab:pink')
-# ax3.plot(x, results_cnn['train_recon_loss'], label='CNN', color='tab:blue')
+    # Plot class loss
+    ax1.set_title('Classification Loss', fontsize = 25)
+    ax1.plot(x, results_base['train_cls_loss'], label='Base', color='tab:olive')
+    ax1.plot(x, results_abs['train_cls_loss'], label='Absolute flow', color='tab:green')
+    ax1.plot(x, results_avg['train_cls_loss'], label='Average flow', color='tab:pink')
+    ax1.plot(x, results_cnn['train_cls_loss'], label='CNN learning', color='tab:blue')
+    ax1.legend()
 
-# plt.tight_layout()
-# plt.savefig(osp.join(save_root,'loss_comparison/Loss_comparison.png'))
+    # Plot feature loss
+    ax2.set_title('Feature Loss', fontsize = 25)
+    ax2.plot(x, results_base['train_feature_loss'], label='Base', color='tab:olive')
+    ax2.plot(x, results_abs['train_feature_loss'], label='Absolute flow', color='tab:green')
+    ax2.plot(x, results_avg['train_feature_loss'], label='Average flow', color='tab:pink')
+    ax2.plot(x, results_cnn['train_feature_loss'], label='CNN', color='tab:blue')
+
+    # Plot reconstruction loss
+    ax3.set_title('Reconstruction Loss', fontsize = 25)
+    ax3.plot(x, results_base['train_recon_loss'], label='Base', color='tab:olive')
+    ax3.plot(x, results_abs['train_recon_loss'], label='Average flow', color='tab:green')
+    ax3.plot(x, results_avg['train_recon_loss'], label='Average flow', color='tab:pink')
+    ax3.plot(x, results_cnn['train_recon_loss'], label='CNN', color='tab:blue')
+
+    plt.tight_layout()
+    os.makedirs(osp.join(save_root,'loss_comparison'), exist_ok=True)
+    plt.savefig(osp.join(save_root,'loss_comparison/Loss_comparison.png'))
 
 
 # ------------------------------
@@ -94,41 +102,56 @@ else:
 def to_percentage(result):
     return [r*100 for r in result]
 
-# Plot the results
-fig, (ax1,ax2) = plt.subplots(1,2, figsize=(20,8))
-fig.suptitle(f'Accuracy Comparison - {fig_title}', fontsize=30)
-for ax in (ax1, ax2):
-    ax.tick_params(axis='both', which='major', labelsize=20)
-    ax.set_xlabel('Epoch', fontsize=20)
-    ax.set_ylim(0, 100) # 0-100% accuracy
-    ax.set_xlim(0, 70)  # 0-70 epochs
-    ax.axvline(x=50, color='black', linestyle='--', alpha=0.2) # Epoch 50
-    ax.axvline(x=60, color='black', linestyle='--', alpha=0.2) # Epoch 60
-    # ax.set_xscale('log')
-ax1.set_ylabel('Classification accuracy %', fontsize=20)
+def plot_accuracy(fig_title: str, x, save_root,
+                  results_base, results_abs, results_avg, results_cnn):
+    """
+    Plot the accuracy comparison graph for different models.
+    Args:
+        fig_title (str): Title of the figure.
+        x (array): Epochs.
+        save_root (str): Path to save the plot.
+        results_base (dict): Results for the base model.
+        results_abs (dict): Results for the absolute flow model.
+        results_avg (dict): Results for the average flow model.
+        results_cnn (dict): Results for the CNN model.
+    """
+    # Create figure and axes
+    fig, (ax1,ax2) = plt.subplots(1,2, figsize=(20,8))
+    fig.suptitle(f'Accuracy Comparison - {fig_title}', fontsize=30)
+    for ax in (ax1, ax2):
+        ax.tick_params(axis='both', which='major', labelsize=20)
+        ax.set_xlabel('Epoch', fontsize=20)
+        ax.set_ylim(0, 100) # 0-100% accuracy
+        ax.set_xlim(0, 70)  # 0-70 epochs
+        ax.axvline(x=50, color='black', linestyle='--', alpha=0.2) # Epoch 50
+        ax.axvline(x=60, color='black', linestyle='--', alpha=0.2) # Epoch 60
+        # ax.set_xscale('log')
+    ax1.set_ylabel('Classification accuracy %', fontsize=20)
 
-# Plot train Accuracy
-ax1.plot(x, to_percentage(results_base['train_AUC']), label='Base', color='tab:olive')
-ax1.plot(x, to_percentage(results_abs['train_AUC']), label='Absolute flow', color='tab:green')
-ax1.plot(x, to_percentage(results_avg['train_AUC']), label='Average flow', color='tab:pink')
-ax1.plot(x, to_percentage(results_cnn['train_AUC']), label='CNN', color='tab:blue')
-ax1.set_title('Train accuracy', fontsize=25)
-ax1.legend(loc=4, prop={'size':20})
+    # Plot train Accuracy
+    ax1.plot(x, to_percentage(results_base['train_AUC']), label='Base', color='tab:olive')
+    ax1.plot(x, to_percentage(results_abs['train_AUC']), label='Absolute flow', color='tab:green')
+    ax1.plot(x, to_percentage(results_avg['train_AUC']), label='Average flow', color='tab:pink')
+    ax1.plot(x, to_percentage(results_cnn['train_AUC']), label='CNN', color='tab:blue')
+    ax1.set_title('Train accuracy', fontsize=25)
+    ax1.legend(loc=4, prop={'size':20})
 
-# Plot test Accuracy
-ax2.plot(x, to_percentage(results_base['test_AUC']), label='Base', color='tab:olive')
-ax2.plot(x, to_percentage(results_abs['test_AUC']), label='Absolute flow', color='tab:green')
-ax2.plot(x, to_percentage(results_avg['test_AUC']), label='Average flow', color='tab:pink')
-ax2.plot(x, to_percentage(results_cnn['test_AUC']), label='CNN', color='tab:blue')
-ax2.set_title('Test accuracy', fontsize=25)
+    # Plot test Accuracy
+    ax2.plot(x, to_percentage(results_base['test_AUC']), label='Base', color='tab:olive')
+    ax2.plot(x, to_percentage(results_abs['test_AUC']), label='Absolute flow', color='tab:green')
+    ax2.plot(x, to_percentage(results_avg['test_AUC']), label='Average flow', color='tab:pink')
+    ax2.plot(x, to_percentage(results_cnn['test_AUC']), label='CNN', color='tab:blue')
+    ax2.set_title('Test accuracy', fontsize=25)
 
-for name, results in {'Base': results_base, 
-                      'Absolute': results_abs, 
-                      'Average': results_avg, 
-                      'CNN': results_cnn}.items():
-    print(f"{name}: {max(to_percentage(results['test_AUC'])):.2f}%")
+    for name, results in {'Base': results_base, 
+                        'Absolute': results_abs, 
+                        'Average': results_avg, 
+                        'CNN': results_cnn}.items():
+        print(f"{name}: {max(to_percentage(results['test_AUC'])):.2f}%")
 
-plt.savefig(osp.join(save_root,'accuracy/Acc_comparison_simple.png'))
+    os.makedirs(osp.join(save_root,'accuracy'), exist_ok=True)
+    plt.savefig(osp.join(save_root,'accuracy/Acc_comparison_simple.png'))
+
 
 # # ------------------------------
 # #   Individual loss graphing
@@ -206,3 +229,9 @@ plt.savefig(osp.join(save_root,'accuracy/Acc_comparison_simple.png'))
 # # ------------------------------
 # # Training time
 # # ------------------------------
+
+
+if __name__ == '__main__':
+    plot_loss_comp(fig_title, x, save_root, results_base, results_abs, results_avg, results_cnn)
+    plot_accuracy(fig_title, x, save_root, results_base, results_abs, results_avg, results_cnn)
+    # plt.show()
