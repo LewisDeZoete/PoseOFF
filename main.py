2457023bd0324d814cf1122f102b77f9f7622d32
LewisDeZoete <lewis.dezoete@gmail.com>
2025-08-18 14:32:35 +1000

    Added argument so main can both train and evaluate a trained model

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ from model import ModelLoader
 from config.argclass import ArgClass
 
 from training.train_infogcn import train_network
+from training.eval_infogcn import eval_network
 from training.loss import LabelSmoothingCrossEntropy, masked_recon_loss
 # from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 parser = argparse.ArgumentParser()
@@ -52,7 +56,8 @@ parser.add_argument(
     help="Description of what is being tested in run",
 )
 parser.add_argument(
-    "--data_path_overwrite", help="Overwrite dataset path"
+    "--data_path_overwrite", help="Overwrite dataset path.\
+    This overwrites `config['data_paths'][arg.evaluation]` completely (must be a file)."
 )
 parser.add_argument(
     "-v", dest="verbose", action="store_true", help="Print verbose output for argparse"
@@ -68,9 +73,11 @@ arg = ArgClass(arg=parsed, verbose=parsed.verbose)
 # Pass root path for the dataset objects
 if arg.data_path_overwrite is not None:
     arg.feeder_args['use_mmap'] = True
-    for arg_key, arg_val in arg.feeder_args['data_paths'].items():
-        arg.feeder_args['data_paths'][arg_key] = osp.join(arg.data_path_overwrite,
-                                                          arg_val.split('/')[-1])
+    arg.feeder_args['data_paths'][arg.evaluation] = arg.data_path_overwrite
+    # for arg_key, arg_val in arg.feeder_args['data_paths'].items():
+    #     arg.feeder_args['data_paths'][arg_key] = 
+    #     arg.feeder_args['data_paths'][arg_key] = osp.join(arg.data_path_overwrite,
+    #                                                       arg_val.split('/')[-1])
 
 # Define checkpoint file (this is the same for train and eval)
 assert arg.run_name != ""
@@ -83,7 +90,7 @@ arg.checkpoint_file = osp.join(  # results/{dataset}/{eval}/train/{run}.pt
 # TODO: rework main.py to handle eval as well as train!
 if arg.phase == "eval":
     arg.eval_save_name = osp.join(  # results/{dataset}/{eval}/eval/{run}.pt
-        arg.save_locastion,
+        arg.save_location,
         arg.evaluation,
         "eval",
         arg.run_name + ".pt"
@@ -105,20 +112,19 @@ device = torch.device(arg.device if torch.cuda.is_available() else "cpu")
 
 # # Create the datasets and dataloaders
 feeder_class = arg.import_class(arg.feeder)
-train_dataset = feeder_class(
-    **arg.feeder_args, eval=arg.evaluation, split="train")
-
+if arg.phase == "train":
+    train_dataset = feeder_class(
+        **arg.feeder_args, eval=arg.evaluation, split="train")
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=arg.batch_size,
+        num_workers=4,
+        shuffle=True,
+        pin_memory=True,
+    )
 
 test_dataset = feeder_class(
     **arg.feeder_args, eval=arg.evaluation, split="test")
-
-train_dataloader = DataLoader(
-    train_dataset,
-    batch_size=arg.batch_size,
-    num_workers=4,
-    shuffle=True,
-    pin_memory=True,
-)
 test_dataloader = DataLoader(
     test_dataset,
     batch_size=arg.batch_size,
@@ -129,25 +135,26 @@ test_dataloader = DataLoader(
 
 print("### Dataloaders created")
 
-# Get the parameters to optimise
-param_groups = {"params": []}
-for name, params in modelLoader.model.named_parameters():
-    param_groups["params"].append(params)
-params = list({"other": param_groups}.values())
+if arg.phase == "train":
+    # Get the parameters to optimise
+    param_groups = {"params": []}
+    for name, params in modelLoader.model.named_parameters():
+        param_groups["params"].append(params)
+        params = list({"other": param_groups}.values())
 
-# Create the optimiser
-optimiser = optim.SGD(
-    params,
-    lr=arg.optim["base_lr"],
-    momentum=0.9,
-    nesterov=arg.optim["nesterov"],
-    weight_decay=arg.optim["weight_decay"],
-)
+    # Create the optimiser
+    optimiser = optim.SGD(
+        params,
+        lr=arg.optim["base_lr"],
+        momentum=0.9,
+        nesterov=arg.optim["nesterov"],
+        weight_decay=arg.optim["weight_decay"],
+    )
 
-# MultiStep Learning Rate scheduler (default steps at 50, 60 epochs)
-scheduler = optim.lr_scheduler.MultiStepLR(
-    optimiser, milestones=arg.optim["step"], gamma=arg.optim["gamma"]
-)
+    # MultiStep Learning Rate scheduler (default steps at 50, 60 epochs)
+    scheduler = optim.lr_scheduler.MultiStepLR(
+        optimiser, milestones=arg.optim["step"], gamma=arg.optim["gamma"]
+    )
 
 # Create the loss function(s)
 cls_loss = LabelSmoothingCrossEntropy(T=arg.model_args["T"])
@@ -159,24 +166,38 @@ loss_funcs = {"cls_loss": cls_loss, "recon_loss": recon_loss}
 # class, feature and reconstruction losses
 score_funcs = ["ACC", "AUC", "cls_loss", "feature_loss", "recon_loss"]
 
-# Simultaneous training of the network
-results = train_network(
-    arg=arg,
-    model=skel_model,
-    loss_funcs=loss_funcs,
-    train_loader=train_dataloader,
-    test_loader=test_dataloader,
-    score_funcs=score_funcs,
-    device=device,
-    epochs=arg.num_epoch,
-    scheduler=scheduler,
-    optimiser=optimiser,
-    checkpoint_file=arg.checkpoint_file,
-    checkpoint_freq=arg.checkpoint_freq,
-    verbose=arg.verbose,
-)
-
-print(f"\tTraining time: \
-{datetime.timedelta(seconds=int(sum(results['train_time']+results['test_time'])))}")
-print(f"\tBest train AUC: {torch.tensor(results['train_AUC']).max().item()}")
-print(f"\tBest test AUC: {torch.tensor(results['test_AUC']).max().item()}")
+# Train or eval phases
+if arg.phase == "train":
+    # Simultaneous training of the network
+    results = train_network(
+        arg=arg,
+        model=skel_model,
+        loss_funcs=loss_funcs,
+        train_loader=train_dataloader,
+        test_loader=test_dataloader,
+        score_funcs=score_funcs,
+        device=device,
+        epochs=arg.num_epoch,
+        scheduler=scheduler,
+        optimiser=optimiser,
+        checkpoint_file=arg.checkpoint_file,
+        checkpoint_freq=arg.checkpoint_freq,
+        verbose=arg.verbose,
+    )
+    print(f"\tTraining time: \
+        {datetime.timedelta(seconds=int(sum(results['train_time']+results['test_time'])))}")
+    print(f"\tBest train AUC: {torch.tensor(results['train_AUC']).max().item()}")
+    print(f"\tBest test AUC: {torch.tensor(results['test_AUC']).max().item()}")
+elif arg.phase == "eval":
+    results = eval_network(
+        arg=arg,
+        model=skel_model,
+        loss_funcs=loss_funcs,
+        test_loader=test_dataloader,
+        checkpoint_file=arg.checkpoint_file,
+        score_funcs=score_funcs,
+        device=device,
+        save_attention=False,  # TODO: handle save_attention for main.py
+    )
+    # Unlike the train loop, the eval loop doesn't have a checkpoint save
+    torch.save(results, arg.eval_save_name)
